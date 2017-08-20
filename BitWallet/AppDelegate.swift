94cec893d598b17f2881c253d60ede1dfcdfeba2
hadadach <hadadach@bowdoin.edu>
2017-08-20 14:12:20 -0700

    Initial Commit

---
+++ BitWallet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BitWallet
+//
+//  Created by Huma Dadachanji on 8/20/17.
+//  Copyright © 2017 Huma Dadachanji. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BitWallet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BitWallet
+//
+//  Created by Huma Dadachanji on 8/20/17.
+//  Copyright © 2017 Huma Dadachanji. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
